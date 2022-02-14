a7dec404972b0a534d87375998141cee05ad9ad0
rafe <rafe@clarusway.com>
2022-02-14 21:49:41 +0100

    added tests

--- hashing/tests.py
+++ hashing/tests.py
@@ -1,27 +1,52 @@
+from hashing.forms import HashForm
 from django.test import TestCase
 from selenium import webdriver
 import time
+import hashlib
+from .models import Hash
 
 class UnitTestCase(TestCase):
-    pass
-
-
-class FunctionalTestCase(TestCase):
-    def setUp(self):
-        self.browser = webdriver.Firefox()
     
-    def tearDown(self):
-        self.browser.quit()
+    def test_home_page_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'hashing/home.html')
         
-    def test_there_is_homepage(self):
-        self.browser.get('http://localhost:8000')
-        self.assertIn("Enter hash here", self.browser.page_source)
+    def test_hash_form(self):
+        form = HashForm(data={'text':'hello'})
+        self.assertTrue(form.is_valid())
+        
+    def test_hash_function(self):
+        hash = hashlib.sha256('hello'.encode('utf-8')).hexdigest()
+        self.assertEqual('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', hash)
+    
+    def test_model_hash(self):
+        hash = Hash()
+        hash.text = 'hello'
+        hash.hash = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824'
+        hash.save()
+        
+        pulled_hash = Hash.objects.get(hash='2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
+        
+        self.assertEqual(hash.text, pulled_hash.text)
+    
 
-    def test_hash_of_hello(self):
-        self.browser.get('http://localhost:8000')
-        text = self.browser.find_element_by_id('id_text')
-        text.send_keys('hello')
-        time.sleep(1)
-        self.browser.find_element_by_name('submit').click()
-        self.assertInHTML('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
-        time.sleep(2)
+
+# class FunctionalTestCase(TestCase):
+#     def setUp(self):
+#         self.browser = webdriver.Firefox()
+    
+#     def tearDown(self):
+#         self.browser.quit()
+        
+#     def test_there_is_homepage(self):
+#         self.browser.get('http://localhost:8000')
+#         self.assertIn("Enter hash here", self.browser.page_source)
+
+#     def test_hash_of_hello(self):
+#         self.browser.get('http://localhost:8000')
+#         text = self.browser.find_element_by_id('id_text')
+#         text.send_keys('hello')
+#         time.sleep(1)
+#         self.browser.find_element_by_name('submit').click()
+#         self.assertInHTML('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
+#         time.sleep(2)
