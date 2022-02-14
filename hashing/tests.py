5c510109231007b0ab864e2b3faa7c0fde7f1350
rafe <rafe@clarusway.com>
2022-02-14 21:56:07 +0100

    added unit test for view hash

--- hashing/tests.py
+++ hashing/tests.py
@@ -24,29 +24,33 @@ class UnitTestCase(TestCase):
         hash.text = 'hello'
         hash.hash = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824'
         hash.save()
-        
         pulled_hash = Hash.objects.get(hash='2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
-        
         self.assertEqual(hash.text, pulled_hash.text)
-    
-
-
-# class FunctionalTestCase(TestCase):
-#     def setUp(self):
-#         self.browser = webdriver.Firefox()
-    
-#     def tearDown(self):
-#         self.browser.quit()
         
-#     def test_there_is_homepage(self):
-#         self.browser.get('http://localhost:8000')
-#         self.assertIn("Enter hash here", self.browser.page_source)
+    def test_viewing_hash(self):
+        hash = Hash()
+        hash.text = 'hello'
+        hash.hash = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824'
+        hash.save()
+        response = self.client.get('/hash/2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
+        self.assertContains(response,'hello')
+    
+class FunctionalTestCase(TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self):
+        self.browser.quit()
+        
+    def test_there_is_homepage(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn("Enter hash here", self.browser.page_source)
 
-#     def test_hash_of_hello(self):
-#         self.browser.get('http://localhost:8000')
-#         text = self.browser.find_element_by_id('id_text')
-#         text.send_keys('hello')
-#         time.sleep(1)
-#         self.browser.find_element_by_name('submit').click()
-#         self.assertInHTML('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
-#         time.sleep(2)
+    def test_hash_of_hello(self):
+        self.browser.get('http://localhost:8000')
+        text = self.browser.find_element_by_id('id_text')
+        text.send_keys('hello')
+        time.sleep(1)
+        self.browser.find_element_by_name('submit').click()
+        self.assertInHTML('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', self.browser.page_source)
+        time.sleep(2)
